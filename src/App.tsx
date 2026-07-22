51acb50a716865ba9fb0ee1350e92cfc34650e65
gmnayem631 <gmnayem631@gmail.com>
2026-07-22 10:09:45 +0600

    feat: add HeroUI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Button } from "@heroui/react";
 
 function App() {
   return (
     <>
       <div className="text-2xl font-bold">CareerTrack Lite</div>
+      Hello
+      <Button className={"bg-red-500"}>HeroUI</Button>
     </>
   );
 }
