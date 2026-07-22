d2381f08e11b26d56980716605b45f3c187182fb
gmnayem631 <gmnayem631@gmail.com>
2026-07-22 12:35:52 +0600

    feat: add Private Route file

---
+++ src/routes/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../../hooks/useAuth";
+import { Spinner } from "@heroui/react";
+import type { ReactNode } from "react";
+
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return <Spinner className="text-blue-500" />;
+  }
+  if (!user) {
+    return <Navigate to={"/auth/login"} state={{ from: location }} replace />;
+  }
+  return children;
+};
+
+export default PrivateRoute;
